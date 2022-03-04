26b83d13597bba2db148ffbd5e4724456ba954d5
MRDGH2821 <mihirrabade@gmail.com>
2022-03-04 13:30:46 +0530

    feat: show client user tag, add JSDocs

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,16 @@
-import { Event } from '@ruinguard/core';
+// eslint-disable-next-line no-unused-vars
+import { Client, Event } from '@ruinguard/core';
 
 export default new Event({
   event: 'ready',
   once: true,
 
-  run() {
-    console.log('ready!');
+  /**
+   * Shows client user tag in console
+   * @function run
+   * @param {Client} client - client object
+   */
+  run(client) {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
   },
 });
